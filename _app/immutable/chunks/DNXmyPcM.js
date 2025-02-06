b714cdf01c9844780b061a99cd9fb87e11e9e855
Dima Kuchin <dima@kuchin.net>
2025-02-06 15:26:45 +0200

    Updates

--- _app/immutable/chunks/DNXmyPcM.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_iijg6p)==null?void 0:s.base)??"/strawberry-bench";var e;const t=((e=globalThis.__sveltekit_iijg6p)==null?void 0:e.assets)??a;export{t as a,a as b};
---
+++ _app/immutable/entry/start.BS3Rxw-e.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/qHSjcxjc.js";export{t as start};
